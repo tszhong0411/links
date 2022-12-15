e7643e76278386e021d348c8a51b423ef6666c61
tszhong0411 <lainelson411@gmail.com>
2022-12-15 18:06:20 +0800

    refactor: rename subdomain

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -15,23 +15,23 @@ const Layout = (props: LayoutProps) => {
   return (
     <>
       <NextSeo
-        titleTemplate='%s | 小康 Link'
-        defaultTitle='小康 Link'
+        titleTemplate='%s | 小康 Links'
+        defaultTitle='小康 Links'
         description='A teenager who want to become a frontend developer'
-        canonical={`https://link.honghong.me${asPath}`}
+        canonical={`https://links.honghong.me${asPath}`}
         twitter={{
           cardType: 'summary_large_image',
           site: '@TszhongLai0411',
           handle: '@TszhongLai0411',
         }}
         openGraph={{
-          url: `https://link.honghong.me${asPath}`,
+          url: `https://links.honghong.me${asPath}`,
           type: 'website',
-          title: '小康 Link',
+          title: '小康 Links',
           description: 'A teenager who want to become a frontend developer',
           images: [
             {
-              url: 'https://link.honghong.me/static/images/banner.png',
+              url: 'https://links.honghong.me/static/images/banner.png',
               width: 1200,
               height: 630,
               alt: 'A teenager who want to become a frontend developer',
