62e1d15f33e36d7e1d4e3af62d07096385ee96ca
tszhong0411 <me@honghong.me>
2025-01-25 17:57:27 +0800

    chore: update postcss config to use postcss-lightningcss, remove autoprefixer dependent

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,9 @@
+/** @type {import('postcss-load-config').Config} */
 export default {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {}
+    'postcss-lightningcss': {
+      browsers: '>= .25%'
+    }
   }
 }
